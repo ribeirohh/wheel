4ad95484688d6ec025b01622190b7aca9a823023
Amanda <amandaribeirinho@outlook.es>
2021-04-28 15:07:30 +0200

    cambios

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const nombres = ["Alisa","Amanda","Ana G","Ana L", "Angie"];
+
+//document.querySelector("button").addEventListener("click",function () {
+
+  function voluntario(){
+    const randomItem = nombres[Math.floor(Math.random()*nombres.length)];
+    console.log(randomItem);
+
+  }
+
+//console.log(nombres[4]);
+
+  console.log(voluntario());
+
+
+
+
+
+
+
+
+
+
+
